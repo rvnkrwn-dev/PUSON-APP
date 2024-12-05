c84b590e858b517b3f4740920fd0886159abe22e
Revan Kurniawan <rvnkrwn@hotmail.com>
2024-12-05 17:50:01 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './node_modules/preline/preline.js',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('preline/plugin'),
+  ],
+}
