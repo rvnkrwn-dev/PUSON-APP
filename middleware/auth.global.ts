c1ce12c375a141c5b971de475b74acba8aeb5fb4
Revan Kurniawan <rvnkrwn@hotmail.com>
2025-01-02 12:50:55 +0700

    update

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -1,6 +1,6 @@
 export default defineNuxtRouteMiddleware((to, from) => {
     const isAuthenticated = useCookie('isLoggedIn').value;
-    const isAuthPage = to.path === '/auth/login' || to.path === '/auth/register' || to.path === '/auth/forget-password';
+    const isAuthPage = to.path === '/auth/login' || to.path === '/auth/register' || to.path === '/auth/forget-password' || to.path === '/auth/reset-password';
 
     if (isAuthPage && isAuthenticated) {
         return navigateTo('/')
